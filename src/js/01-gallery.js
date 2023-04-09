416754df566be81c5a53dc401c5b1a055493e057
AndrijKarmaza <andrij.karmaza@gmail.com>
2023-04-09 14:54:24 +0300

    homework is done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const marcupGallery = galleryItems.map(({preview, original, description}) => `
+<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`).join('');
+
+gallery.insertAdjacentHTML('beforeend', marcupGallery);
+gallery.addEventListener('click', onImageClick);
+
+let modal = new SimpleLightbox('.gallery a', 
+    {captionsData: 'alt', captionDelay: 250, fadeSpeed: 250,
+    scrollZoom: false, disableRightClick: true});
+
+function onImageClick(evt) {
+    evt.preventDefault();
+
+    if (!evt.target.classList.contains('gallery__image')) {
+        return;        
+    }
+    
+    modal.on('show.simplelightbox');
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
+
+const userStrogateKey = 'feedback-form-state';
+let userData = {};
+
+form.addEventListener('submit', onSubmit);
+form.addEventListener('input', throttle(onInput, 500));
+
+
+savedData();
+
+function onSubmit(evt) {
+    evt.preventDefault();
+    userData = JSON.parse(localStorage.getItem(userStrogateKey));
+    console.log(userData);
+    evt.currentTarget.reset();
+    localStorage.removeItem(userStrogateKey);
+}
+
+function onInput(evt) {
+    userData[evt.target.name] = evt.target.value;
+    localStorage.setItem(userStrogateKey, JSON.stringify(userData));
+}
+
+function savedData() {
+    const data = JSON.parse(localStorage.getItem(userStrogateKey));
+    if (data) {
+        if (data.email) {
+            email.value = data.email;
+            userData.email = data.email;
+        }
+        if (data.message) {
+            message.value = data.message;
+            userData.message = data.message;
+        } 
+    }
+}
